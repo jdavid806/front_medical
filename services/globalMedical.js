11e80a84912bd0cae2df39128dd099f8b6b72ae9
jdavid8062 <jeffersondavila1101@gmail.com>
2025-03-09 09:20:19 -0500

    configure docker

--- services/globalMedical.js
+++ services/globalMedical.js
@@ -1,6 +1,14 @@
+// const domain = window.location.hostname; // Obtiene el subdominio dinámico
+// const api = "/";
+// const url = `${domain}${api}`;
+// export { url };
+
+
 const domain = window.location.hostname; // Obtiene el subdominio dinámico
 const api = "/";
+const port = "8000";
 
-const url = `${domain}${api}`;
 
+const url = `${domain}:${port}${api}`;
+console.log(url);
 export { url };
